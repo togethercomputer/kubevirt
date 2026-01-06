d47b01428b9f2f975756d2c4935fc95033260a13
Bogdan Luca <bogdan@together.ai>
2026-01-06 16:24:24 +0200

    fix(virt-controller): sort interfaces before deep equal comparison (#8)
    * fix(virt-controller): sort interfaces before deep equal comparison
    
    * chore: update comment
    
    * feat: add build and push script
    
    * feat(build-push): allow setting DOCKER_TAG

---
+++ build-push.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# this script depends on being logged in to the "together.ai - prod" public
+# ECR registry; to login run
+# aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws
+
+export DOCKER_PREFIX='public.ecr.aws/k6t4m3l7/kubevirt'
+DOCKER_TAG="${DOCKER_TAG:-"$(git describe --tags)"}"
+export DOCKER_TAG
+PUSH_TARGETS="${PUSH_TARGETS:-virt-controller}"
+
+read -rp "Build tag $DOCKER_TAG for ${PUSH_TARGETS// /, } image(s)? [y/N] " response
+
+if [ "$response" = 'y' ] || [ "$response" = 'Y' ]; then
+  make bazel-push-images PUSH_TARGETS="$PUSH_TARGETS"
+fi
