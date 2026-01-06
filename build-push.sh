a38e5cc6d40874f28fcb61da79fe05382c94c9c8
Bogdan Luca <bogdan@together.ai>
2026-01-06 16:22:36 +0200

    feat(build-push): allow setting DOCKER_TAG

--- build-push.sh
+++ build-push.sh
@@ -7,9 +7,9 @@ set -euo pipefail
 # aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws
 
 export DOCKER_PREFIX='public.ecr.aws/k6t4m3l7/kubevirt'
-DOCKER_TAG="$(git describe --tags)"
+DOCKER_TAG="${DOCKER_TAG:-"$(git describe --tags)"}"
 export DOCKER_TAG
-PUSH_TARGETS="${PUSH_TARGETS:-"virt-controller"}"
+PUSH_TARGETS="${PUSH_TARGETS:-virt-controller}"
 
 read -rp "Build tag $DOCKER_TAG for ${PUSH_TARGETS// /, } image(s)? [y/N] " response
 
